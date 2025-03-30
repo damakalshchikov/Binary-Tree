813d491d3bc47706b65b7b415e8b5f67adacb701
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-03-30 20:07:49 +0300

    Рефакторинг

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from Functions.work_with_tree import appropriate_descendants, read_file, draw_tr
 
 
 def main():
+
+    # Печать приветственной информации и ввод источника данных
     print("Данная программа находит длиннейший путь(пути),\nвдоль которого номера вершин упорядочены по возрастанию\n")
     file_name: str = input("Введите имя файла для считывания входных данных: ")
 
-
+    # Вызов ф-ии, которая преобразует текстовый файл в массив с вершинами дерева
     tree: list[Node | None] = read_file(file_name)
 
-
+    # Вызов ф-ии, которая устанавливает связь между вершинами в соответствии с исходными данными
     appropriate_descendants(tree)
 
     for index, node in enumerate(tree):
@@ -18,6 +20,9 @@ def main():
             continue
 
         print(f"Вершина {node}. Левый потомок {node.left}, правый потомок {node.right}")
+
+    # Вызов ф-ии, которая создаёт изображение бинарного дерева
+    draw_tree(tree)
 
 
 if __name__ == "__main__":
