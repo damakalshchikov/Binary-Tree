7ff4c0539d54e1acd1dbed46477ad3a50b77d757
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-03-31 18:11:26 +0300

    Вызов ф-ии визуализации найденных путей

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Classes.Node import Node
-from Functions.work_with_tree import appropriate_descendants, read_file, draw_tree, find_longest_path
+from Functions.work_with_tree import appropriate_descendants, read_file, draw_tree, find_longest_path, draw_path
 
 
 def main():
@@ -37,6 +37,8 @@ def main():
         for i, path in enumerate(longest_paths, 1):
             path_str = " -> ".join(str(node.number) for node in path)
             print(f"Путь {i}: {path_str} (длина: {len(path)})")
+            # Вызов ф-ии, которая визуализирует найденные пути
+            draw_path(tree, path, output_png=f"./Images/path{i}.png")
 
 
 if __name__ == "__main__":
